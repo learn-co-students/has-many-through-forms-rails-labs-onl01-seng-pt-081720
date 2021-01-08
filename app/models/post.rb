3135814a051a66f32d0a745cfb91d1bb6e773a19
Brenda Ferrufino <ferrufinob@gmail.com>
2021-01-08 00:51:18 -0500

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,7 @@ class Post < ActiveRecord::Base
   has_many :categories, through: :post_categories
   has_many :comments
   has_many :users, through: :comments
-  #this method does not prevent duplicated from being created
+  #this method does not prevent duplication!!!
   # accepts_nested_attributes_for :categories, reject_if: :all_blank
 
   #what the accepts_nested_attributes_for method is doing behind the scenes
@@ -12,7 +12,7 @@ class Post < ActiveRecord::Base
   def categories_attributes=(categories_attributes)
     categories_attributes.values.each do |category_attribute|
       if !category_attribute[:name].blank?
-        #if i pass in find or create with key/value method doesnt seem to work and allows duplication
+        #if i pass in find or create with key/value, method doesn't seem to work and allows duplication
         category = Category.find_or_create_by(category_attribute)
         self.categories << category
       end
