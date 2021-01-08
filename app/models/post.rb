dbd37a963abe421f8ec92333d3fa3608fe2b08f6
Brenda Ferrufino <ferrufinob@gmail.com>
2021-01-07 20:00:05 -0500

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,14 @@ class Post < ActiveRecord::Base
   has_many :categories, through: :post_categories
   has_many :comments
   has_many :users, through: :comments
+  # accepts_nested_attributes_for :categories, reject_if: :all_blank
 
-  accepts_nested_attributes_for :categories, reject_if: :all_blank
+  def categories_attributes=(categories_attributes)
+    categories_attributes.values.each do |category_attribute|
+      if !category_attribute[:name].blank?
+        category = Category.find_or_create_by(name: category_attribute[:name])
+        self.categories << category
+      end
+    end
+  end
 end
