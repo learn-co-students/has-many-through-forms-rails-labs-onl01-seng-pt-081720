e468aa1963e429d14ed866874753edfc31db99b6
tonitdiep <adiep123@gmail.com>
2021-01-28 03:50:16 -0500

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,9 +9,11 @@ class PostsController < ApplicationController
 
   def new
     @post = Post.new
+    # raise @post.inspect
   end
 
   def create
+  
     post = Post.create(post_params)
     redirect_to post
   end
